93ed53a9aa3920ea5d53073522986fec3375e04e
Luclu7 <me@luclu7.fr>
2021-05-30 00:27:49 +0200

    owo c'est en parallèle maintenant

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 import os
 import configparser
 import json
+import threading
 
 parser = argparse.ArgumentParser()
 parser.add_argument("comport", help="selects which COM port to use to speak to the Arduino", default="COM46")
@@ -19,8 +20,6 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 keys = config[loco_name[1]]
 
-lastDataSent = ""
-
 
 class Object:
     def toJSON(self):
@@ -30,61 +29,85 @@ class Object:
 if False:
     while True:
         print("CG_control: " + str(
-            rd.get_current_controller_value("CG_control"))+ " BrakePipePressureBAR: " + str(
+            rd.get_current_controller_value("CG_control")) + " BrakePipePressureBAR: " + str(
             rd.get_current_controller_value("BrakePipePressureBAR")) + " TrainBrakeCylinderPressureBAR: " + str(
             rd.get_current_controller_value("TrainBrakeCylinderPressureBAR")))
         time.sleep(1 / 30)
 
-with Serial(port=args.comport, baudrate=115200, timeout=1, writeTimeout=1) as port_serie:
+port_serie = Serial(port=args.comport, baudrate=115200, timeout=1, writeTimeout=1)
+
+
+def readInput():
     if port_serie.isOpen():
+        oldline = ""
         while True:
             ligne = port_serie.readline().rstrip()
-            args = ligne.split(",".encode())
-            try:
-                textToPrint = ""
-                #textToPrint += "Regulator: " + args[0].decode("utf-8")
-                #textToPrint += "Reverser: " + args[1].decode("utf-8")
-                #textToPrint += " VACMA: " + args[2].decode("utf-8")
+            if ligne != oldline:
+                try:
+                    args = json.loads(ligne)
+                    print(args)
+                    textToPrint = ""
+                    # textToPrint += "Regulator: " + args[0].decode("utf-8")
+                    # textToPrint += "Reverser: " + args[1].decode("utf-8")
+                    # textToPrint += " VACMA: " + args[2].decode("utf-8")
 
-                # print(textToPrint)
-                # rd.set_controller_value(keys["Regulator"], float(args[0]))
-                # rd.set_controller_value(keys["Reverser"], float(args[1]))
-                # rd.set_controller_value(keys["VACMA"], float(args[2].decode("utf-8")))
+                    # print(textToPrint)
+                    # rd.set_controller_value(keys["Regulator"], float(args[0]))
+                    # rd.set_controller_value(keys["Reverser"], float(args[1]))
+                    # rd.set_controller_value(keys["VACMA"], float(args[2].decode("utf-8")))
 
-                #rd.set_controller_value(keys["BPSF"], float(args[3].decode("utf-8")))
-                # rd.set_controller_value(keys["TEST"], float(args[4].decode("utf-8")))
-                # rd.set_controller_value(keys["BPFC"], float(args[5].decode("utf-8")))
-                # rd.set_controller_value(keys["BPMV"], float(args[6].decode("utf-8")))
-                # rd.set_controller_value(keys["BPVAL"], float(args[7].decode("utf-8")))
+                    rd.set_controller_value(keys["BPSF"], float(args["SF"]))
+                    rd.set_controller_value(keys["TEST"], float(args["TEST"]))
+                    rd.set_controller_value(keys["BPFC"], float(args["FC"]))
+                    # rd.set_controller_value(keys["BPMV"], float(args[6].decode("utf-8")))
+                    rd.set_controller_value(keys["BPVAL"], float(args["VAL"]))
 
-                # if rd.get_current_controller_value("KVB_BP_VIO_lumiere_control") == 1.0:
-                #    rd.set_controller_value("KVB_BP_VIO_ON_control", float(args[6].decode("utf-8")))
-                # else:
-                #    rd.set_controller_value("KVB_BP_VIO_off_control", float(args[6].decode("utf-8")))
-                # print("KVB_LS_V_control: " + str(
-                #        rd.get_current_controller_value("KVB_LS_V_control")) + " KVB_LS_FU_control: " + str(
-                #        rd.get_current_controller_value("KVB_LS_FU_control")) + " KVB_BP_VIO_lumiere_control: " + str(
-                #        rd.get_current_controller_value("KVB_BP_VIO_lumiere_control")))
-                dataToBeSent = Object()
-                dataToBeSent.visu = str(int(rd.get_current_controller_value("KVB_visu_control")))
-                dataToBeSent.autotest = str(int(rd.get_current_controller_value("Autotest_KVB_control")))
-                dataToBeSent.LSFU = str(int(rd.get_current_controller_value("KVB_LS_FU_control")))
-                dataToBeSent.LSV = str(int(rd.get_current_controller_value("KVB_LS_V_control")))
-                if lastDataSent != dataToBeSent.toJSON():
-                    print(dataToBeSent.toJSON())
-                    port_serie.write(str.encode(dataToBeSent.toJSON() + "\n"))
-                    lastDataSent=dataToBeSent.toJSON()
-                # print(str.encode('{"visu":' +  + '}\n'))
-            except ValueError as e:
-                print("ValueError")
-                print(e)
-            except IndexError as e:
-                print("IndexError")
-                print(e)
+                    # if rd.get_current_controller_value("KVB_BP_VIO_lumiere_control") == 1.0:
+                    #    rd.set_controller_value("KVB_BP_VIO_ON_control", float(args[6].decode("utf-8")))
+                    # else:
+                    #    rd.set_controller_value("KVB_BP_VIO_off_control", float(args[6].decode("utf-8")))
+                    # print("KVB_LS_V_control: " + str(
+                    #        rd.get_current_controller_value("KVB_LS_V_control")) + " KVB_LS_FU_control: " + str(
+                    #        rd.get_current_controller_value("KVB_LS_FU_control")) + " KVB_BP_VIO_lumiere_control: " + str(
+                    #        rd.get_current_controller_value("KVB_BP_VIO_lumiere_control")))
+                    # print(str.encode('{"visu":' +  + '}\n'))
+                except ValueError as e:
+                    print("ValueError", str(e))
+                    print(ligne)
+                except IndexError as e:
+                    print("IndexError")
+                    print(e)
+                oldline = ligne
+
+
+def writeOutput():
+    lastDataSent = ""
+    if port_serie.isOpen():
+        while True:
+            dataToBeSent = Object()
+            dataToBeSent.visu = str(int(rd.get_current_controller_value("KVB_visu_control")))
+            dataToBeSent.autotest = str(int(rd.get_current_controller_value("Autotest_KVB_control")))
+            dataToBeSent.LSFU = str(int(rd.get_current_controller_value("KVB_LS_FU_control")))
+            dataToBeSent.LSV = str(int(rd.get_current_controller_value("KVB_LS_V_control")))
+            dataToBeSent.LSV = str(int(rd.get_current_controller_value("KVB_LS_V_control")))
+            dataToBeSent.FC = str(int(rd.get_current_controller_value("KVB_BP_CAR_lumiere_control")))
+            dataToBeSent.VAL = str(int(rd.get_current_controller_value("KVB_BP_VAL_lumiere_control")))
+
+            data = dataToBeSent.toJSON()
+            # {"visu":7, "autotest":4,"LSFU":1,"ENGIN":1, "LSV": 1, "SOL": 1}
+            if lastDataSent != data:
+                print(data)
+                port_serie.write(str.encode(data + "\n"))
+                lastDataSent = data
+
 
 if loco_name is None:
     print("Can't find any loco")
     os._exit(1)
+
+threading.Thread(target=readInput).start()
+threading.Thread(target=writeOutput).start()
+
 
 print(dict(rd.get_controller_list()).values())
 
